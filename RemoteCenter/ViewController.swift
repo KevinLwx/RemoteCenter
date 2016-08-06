3b2e8a940819797f3f50f0006edd17a62d35f25c
huang <Linkling@huangdeimac.local>
2016-08-06 18:43:18 +0800

    Initial Commit

---
+++ RemoteCenter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteCenter
+//
+//  Created by huang on 16/8/6.
+//  Copyright © 2016年 huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
